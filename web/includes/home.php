bbe42b39339acf54c13f52850b1df280447072d6
Vladimir Orlov <vladimir.orlov.dev@gmail.com>
2025-12-02 21:16:48 -0300

    подправил home

--- web/includes/home.php
+++ web/includes/home.php
@@ -1,6 +1,75 @@
-<div class="profile_page">
-    <div class="profile_container">
-        <h1 class="profile_title">Home</h1>
+<div class="home_page">
+    <div class="home_container">
+        <h1 class="home_title">Mis Cursos</h1>
+        <div class="daily_progress_card">
+            <div class="daily_progress_label">Aprendido hoy</div>
+            <div class="daily_progress_time">
+                <span class="daily_progress_current">46min</span>
+                <span class="daily_progress_total">/60min</span>
+            </div>
+            <div class="daily_progress_bar">
+                <div class="daily_progress_fill" style="width: 76.67%;"></div>
+            </div>
+        </div>
+        <div class="courses_grid">
+            <div class="course_card course_card_pink">
+                <div class="course_card_content">
+                    <h3 class="course_title">HTML desde cero</h3>
+                    <div class="course_progress_bar">
+                        <div class="course_progress_fill" style="width: 58.33%;"></div>
+                    </div>
+                    <div class="course_completion">
+                        <div class="course_completion_text">
+                            <span class="course_completion_label">Completado</span>
+                            <span class="course_completion_count">14/24</span>
+                        </div>
+                        <button class="course_play_button course_play_button_pink" aria-label="Play course">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8 5V19L19 12L8 5Z" fill="white"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="course_card course_card_blue">
+                <div class="course_card_content">
+                    <h3 class="course_title">HTML avanzado</h3>
+                    <div class="course_progress_bar">
+                        <div class="course_progress_fill" style="width: 66.67%;"></div>
+                    </div>
+                    <div class="course_completion">
+                        <div class="course_completion_text">
+                            <span class="course_completion_label">Completado</span>
+                            <span class="course_completion_count">12/18</span>
+                        </div>
+                        <button class="course_play_button course_play_button_blue" aria-label="Play course">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8 5V19L19 12L8 5Z" fill="white"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="course_card course_card_teal">
+                <div class="course_card_content">
+                    <h3 class="course_title">HTML desde cero</h3>
+                    <div class="course_progress_bar">
+                        <div class="course_progress_fill" style="width: 62.5%;"></div>
+                    </div>
+                    <div class="course_completion">
+                        <div class="course_completion_text">
+                            <span class="course_completion_label">Completado</span>
+                            <span class="course_completion_count">10/16</span>
+                        </div>
+                        <button class="course_play_button course_play_button_teal" aria-label="Play course">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8 5V19L19 12L8 5Z" fill="white"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <? include "includes/footer.php"; ?>
 </div>
